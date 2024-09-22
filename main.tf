f600a83e5f13ad620cb999fe503650428b58762f
Jeremy Brown <chase208@4JLT.com>
2024-09-22 10:38:18 -0400

    Trying adding reuqired providers

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ terraform {
       name = "periodic-table-infrastrucutre"
     }
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # Adjust the version if needed
+    }
+  }
 }
 
 provider "aws" {
