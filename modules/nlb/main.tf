1c1cda66c480c7403e00bb7727c4d883483c86ca
Jeremy Brown <chase208@4JLT.com>
2024-10-02 16:38:13 -0400

    Let's update the nlb to add a listener

--- modules/nlb/main.tf
+++ modules/nlb/main.tf
@@ -10,18 +10,29 @@ resource "aws_lb" "periodic_table_nlb" {
 }
 
 resource "aws_lb_target_group" "periodic_table_tg" {
-  name        = "periodic-table-tg"
+  name        = "periodic-table-tg-${var.env}"
   port        = 80
-  protocol    = "HTTP"
+  protocol    = "TCP"
   vpc_id      = var.periodic_table_vcp_id
   target_type = "ip"
 
   health_check {
     path = "/"
-    protocol = "HTTP"
+    protocol = "TCP"
   }
 
   tags = {
     env = "${var.tag}"
+  }
+}
+
+resource "aws_lb_listener" "periodic_table_listener" {
+  load_balancer_arn = aws_lb.periodic_table_nlb.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.periodic_table_tg.id
+    type             = "forward"
   }
 }
