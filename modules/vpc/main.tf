3a70a2d4dfaadc76b28438f1248ee5c4a2006b2f
Jeremy Brown <chase208@4JLT.com>
2025-01-02 15:36:04 -0500

    Updating security group

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "ecs_sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
