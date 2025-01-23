d20fb35a574a659b6ab141ef4ea94d5cdfef9989
Jeremy Brown <chase208@4JLT.com>
2025-01-22 19:07:46 -0500

    Updating cidr blocks to security groups

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "ecs_sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = [aws_security_group.alb_sg.id]
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
