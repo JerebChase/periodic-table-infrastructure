6a3a3bd043a3ffbf6051fc3962b2c83401becc4c
Jeremy Brown <chase208@4JLT.com>
2025-01-22 22:37:35 -0500

    Adding back in lb target group and listener

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -8,7 +8,7 @@ output "periodic_table_lb_dns_name" {
     value       = aws_lb.periodic_table_alb.dns_name
 }
 
-# output "periodic_table_tg_arn" {
-#     description = "The arn for the target group"
-#     value       = aws_lb_target_group.periodic_table_tg.arn
-# }
+output "periodic_table_tg_arn" {
+    description = "The arn for the target group"
+    value       = aws_lb_target_group.periodic_table_tg.arn
+}
--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -62,11 +62,11 @@ resource "aws_ecs_service" "periodic_table_service" {
     assign_public_ip = true
   }
 
-  # load_balancer {
-  #   target_group_arn = var.periodic_table_tg_arn
-  #   container_name   = "periodic-table-container-${var.env}"
-  #   container_port   = 8080
-  # }
+  load_balancer {
+    target_group_arn = var.periodic_table_tg_arn
+    container_name   = "periodic-table-container-${var.env}"
+    container_port   = 8080
+  }
 
   tags = {
     env = "${var.tag}"
