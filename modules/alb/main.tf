91d5870bb23cb59a8ddf19024c0563b96a74c130
Jeremy Brown <chase208@4JLT.com>
2025-01-22 22:30:01 -0500

    Removing target group temp

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -10,26 +10,26 @@ resource "aws_lb" "periodic_table_alb" {
   }
 }
 
-resource "aws_lb_target_group" "periodic_table_tg" {
-  name        = "periodic-table-tg-${var.env}"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = var.periodic_table_vcp_id
-  target_type = "ip"
+# resource "aws_lb_target_group" "periodic_table_tg" {
+#   name        = "periodic-table-tg-${var.env}"
+#   port        = 8080
+#   protocol    = "HTTP"
+#   vpc_id      = var.periodic_table_vcp_id
+#   target_type = "ip"
 
-  health_check {
-    path                = "/health"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200"
-  }
+#   health_check {
+#     path                = "/health"
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     matcher             = "200"
+#   }
 
-  tags = {
-    env = "${var.tag}"
-  }
-}
+#   tags = {
+#     env = "${var.tag}"
+#   }
+# }
 
 # resource "aws_lb_listener" "periodic_table_listener" {
 #   load_balancer_arn = aws_lb.periodic_table_alb.arn
