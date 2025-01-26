18fba0aab62dd2058b3de6c8983ed8043cb486b5
Jeremy Brown <chase208@4JLT.com>
2025-01-26 11:22:40 -0500

    Adding the inbound https rule for the alb security group

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -33,10 +33,20 @@ resource "aws_subnet" "periodic_table_subnet2" {
 resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.periodic_table_vpc.id
 
+  # Allow HTTP traffic
   ingress {
     description = "Allow HTTP traffic"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTPS traffic
+  ingress {
+    description = "Allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
