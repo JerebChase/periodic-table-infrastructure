5b930641a3754a2bcf7febd0aade27c7c984a3d6
Jeremy Brown <chase208@4JLT.com>
2025-01-26 08:46:42 -0500

    Adding http cert and listener to alb

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -31,7 +31,16 @@ resource "aws_lb_target_group" "periodic_table_tg" {
   }
 }
 
-resource "aws_lb_listener" "periodic_table_listener" {
+resource "aws_acm_certificate" "periodic_table_cert" {
+  domain_name       = "periodictable-api-${var.env}.jeremychasebrown.com"
+  validation_method = "DNS"
+
+  tags = {
+    env = "${var.tag}"
+  }
+}
+
+resource "aws_lb_listener" "periodic_table_listener_http" {
   load_balancer_arn = aws_lb.periodic_table_alb.arn
   port              = 80
   protocol          = "HTTP"
@@ -39,5 +48,18 @@ resource "aws_lb_listener" "periodic_table_listener" {
   default_action {
     target_group_arn = aws_lb_target_group.periodic_table_tg.arn
     type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "periodic_table_listener_https" {
+  load_balancer_arn = aws_lb.periodic_table_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.periodic_table_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.periodic_table_tg.arn
   }
 }
