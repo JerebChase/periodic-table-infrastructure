476c19a376dba087a103b0eaa3d71c8ca6436787
Jeremy Brown <chase208@4JLT.com>
2025-02-01 08:38:51 -0500

    Adding vpc endpoint for s3

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -215,6 +215,17 @@ resource "aws_vpc_endpoint" "ecr_docker" {
   }
 }
 
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.periodic_table_vpc.id
+  service_name = "com.amazonaws.us-east-1.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.periodic_table_private_rt.id]
+
+  tags = {
+    env  = "${var.tag}"
+  }
+}
+
 resource "aws_vpc_endpoint" "dynamodb" {
   vpc_id       = aws_vpc.periodic_table_vpc.id
   service_name = "com.amazonaws.us-east-1.dynamodb"
